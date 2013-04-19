760c1f197576682ddba3bc29a77ce3486fa38a6d
rojters <rojters@gmail.com>
2013-04-19 15:56:31 +0200

    add async http response check

--- android-app/breezehome/src/com/example/breezehome/MainActivity.java
+++ android-app/breezehome/src/com/example/breezehome/MainActivity.java
@@ -1,76 +1,155 @@
 package com.example.breezehome;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.view.Menu;
+import android.content.IntentFilter;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.net.Uri;
-import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.wifi.SupplicantState;
 
 public class MainActivity extends Activity {
 
-	WifiManager wifi;
-	WifiInfo wifiInfo;
-	TextView status;
-	WifiConfiguration wifiConf;
-	String breezehomeSSID;
-	String breezehomePass;
-	String breezehomeUrl;
-	String currentSSID;
+	private WifiManager wifi;
+	private WifiInfo wifiInfo;
+	private TextView status;
+	private TextView help;
+	private WifiConfiguration wifiConf;
+	private IntentFilter intentFilter;
+	private String breezehomeSSID;
+	private String breezehomePass;
+	private String breezehomeUrl;
+	private String currentSSID;
+	private boolean disconnectOccurred;
+	
+	// Check if breezehome responds to HTTP requests. 
+	private class BreezeHomeWebCheck extends AsyncTask<String, Void, Boolean> {
+		protected Boolean doInBackground(String... urls) {
+			Log.d("DEBUG", "AsyncTask doInBackground running");
+			String checkURL = urls[0];
+			boolean connectedToBreeze = false;
+				do {
+					try {
+						URL url = new URL(checkURL);
+						HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+						connection.setRequestProperty("User-Agent", "yourAgent");
+						connection.setRequestProperty("Connection", "close");
+						connection.setConnectTimeout(1000);
+						if (connection.getResponseCode() == 200) {
+							connectedToBreeze = true;
+							return true;
+						}
+					} catch (MalformedURLException e) {
+						
+					} catch (IOException e) {
+						
+					}
+				} while (connectedToBreeze == false);
+				return false;
+	     }
+		 
+		 protected void onPostExecute(Boolean result) {
+			 Log.d("DEBUG", "AsyncTask onPostExecute running");
+			 if (result == true) {
+				 Log.d("DEBUG", "AsyncTask onPostExecute opening browser");
+				 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(breezehomeUrl));
+				 startActivity(browserIntent);
+			 } 
+	     }
+	 }
+	
+	
+	// Monitor the wifi state to discover when we have connected to the breezehome access point.
+	public BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+	    @Override
+	    public void onReceive(Context context, Intent intent) {
+	    	final String action = intent.getAction();
+	    	Log.d("DEBUG", "onReceive: " + action);
+	    	if (intent.getAction().equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION)) {
+	    		SupplicantState stateInfo = (SupplicantState) intent.getParcelableExtra(WifiManager.EXTRA_NEW_STATE);
+	    		Log.d("DEBUG", "onReceive [SUPPLICANT_STATE_CHANGED_ACTION] EXTRA_NEW_STATE = " + stateInfo.name());
+	    		if (stateInfo.name() == SupplicantState.DISCONNECTED.name()) {
+	    			disconnectOccurred = true;
+	    		} else if  (stateInfo.name() == SupplicantState.COMPLETED.name()) {
+	    			if (disconnectOccurred == true) {
+	    				disconnectOccurred = false;
+	    				openBrowser(breezehomeUrl);
+	    			}
+	    		}
+	    	} 
+	    }
+	};
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        status = (TextView) findViewById(R.id.textViewStatus);
         
-        // Unless wifi is enabled there is no point to continue.
+        // The following TextViews will be updated in the application life cycle. 
+        status = (TextView) findViewById(R.id.textViewStatus);
+        help = (TextView) findViewById(R.id.textViewHelp);
+    }
+   
+    @Override
+    protected void onResume() {
+        super.onResume();
+		help.setText("Hold the device close to a tag to begin");
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+    	
+    	// Unless wifi is enabled there is no point to continue. 
         if (wifi.isWifiEnabled() == false) {
         	wifi.setWifiEnabled(true);
         }
         
-        // Wait until we get relevant info from NFC/RDID.
-        breezehomeSSID = "\"test\"";
-        breezehomePass = "\"test\"";
+        // TODO: Wait until we get relevant info from NFC/RDID.
+        breezehomeSSID = "\"breezehome\"";
+        breezehomePass = "\"whiterun\"";
         breezehomeUrl = "http://www.google.se";
         
-        // Check if we are connected.
+        // Check if device is already connected to a access point. 
         wifiInfo = wifi.getConnectionInfo();
         if (wifiInfo != null) {
         	currentSSID = wifiInfo.getSSID();
         	status.append(currentSSID);
-        	// Check if we are on the breezehome ap.
+        	// Check if device is already connected to the specified breezehome access point. 
         	if (currentSSID.equalsIgnoreCase(breezehomeSSID)) {
         		openBrowser(breezehomeUrl);
         	}	
         }
     }
     
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    }
+    
     private void openBrowser(String localUrl) {
-    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(localUrl));
-		startActivity(browserIntent);
+    	help.setText("Connecting to breezehome, please wait");
+    	new BreezeHomeWebCheck().execute(localUrl);
     } 
     
+    // TODO: This button will be replaced with a non-interactive task started after NFC/RFID scanning is complete.
     public void authenticate(View view) {
-    	
-    	// Set up the connection configuration.
     	wifiConf = (WifiConfiguration) new WifiConfiguration();
     	wifiConf.SSID = breezehomeSSID;
     	wifiConf.preSharedKey = breezehomePass;
     	wifiConf.hiddenSSID = false;
     	int netID = wifi.addNetwork(wifiConf);
     	wifi.enableNetwork(netID, true);
-    	
-    	// If we are connected to the correct AP we can now go over to the mobile site.
-    	
-    	openBrowser(breezehomeUrl);
+    	intentFilter = new IntentFilter(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+    	registerReceiver(broadcastReceiver, intentFilter);
     }
-    
 }
