21c306e874e5d930ab0a0fde4c0a21f14fdd9ed6
Mårten Blomberg <martenblomberg@gmail.com>
2013-04-22 14:39:05 +0200

    Created web-server
    Assumes the functions play() and pause() in media.py

---
+++ web-server/web-server/webserver.py
@@ -0,0 +1,13 @@
+import cherrypy
+import ../media-player/media
+
+class webserver:
+	def index(self):
+		return <a href="/play">play</a>
+	def index(self, playbtn):
+		if playbtn = "play"
+			play()
+			return <a href="../pause">pause</a>
+		if playbtn = "pause"
+			pause()
+			return <a href="../play">play</a>
