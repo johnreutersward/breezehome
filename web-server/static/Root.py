5627739b02b0ba4283e2fb07f242040665731c1e
nilsi <nilsson.nicklas@live.se>
2013-04-28 19:27:55 +0200

    A start on the interface, more to come

---
+++ web-server/static/Root.py
@@ -0,0 +1,16 @@
+import os
+import cherrypy
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+class Root(object): pass
+
+cherrypy.tree.mount(Root(), '/', config={
+        '/': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': PATH,
+                'tools.staticdir.index': 'index.html',
+            },
+    })
+
+cherrypy.engine.start()
+cherrypy.engine.block()
